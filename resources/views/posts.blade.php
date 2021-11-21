766601e77069378861a252b112e8f2f74963f23d
Alif Delgado <alifdelgado@gmail.com>
2021-11-21 02:19:49 -0600

    Added post model and loading posts with cache

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
+</head>
+<body>
+    <article>
+        <h1><a href="/post/my-first-post">My first post</a></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis earum similique, ratione molestiae repudiandae dignissimos quos sapiente ullam possimus non exercitationem laborum necessitatibus asperiores accusamus et saepe praesentium adipisci sequi!
+        Voluptatem ut repellat dolorum, fugiat nemo mollitia iste id, repudiandae, nisi cum odio ipsam aliquid nostrum. Atque rerum fuga, ipsum consectetur commodi temporibus ipsa enim corrupti eveniet dolores placeat perspiciatis!
+        Omnis eaque itaque illo ab quae odio dolorem nesciunt voluptas, quaerat tempore inventore vitae quam quasi fuga sequi veniam eius porro. Assumenda illum sequi magni laboriosam eligendi quod nobis alias.</p>
+    </article>
+    <article>
+        <h1><a href="/post/my-second-post">My second post</a></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis earum similique, ratione molestiae repudiandae dignissimos quos sapiente ullam possimus non exercitationem laborum necessitatibus asperiores accusamus et saepe praesentium adipisci sequi!
+        Voluptatem ut repellat dolorum, fugiat nemo mollitia iste id, repudiandae, nisi cum odio ipsam aliquid nostrum. Atque rerum fuga, ipsum consectetur commodi temporibus ipsa enim corrupti eveniet dolores placeat perspiciatis!
+        Omnis eaque itaque illo ab quae odio dolorem nesciunt voluptas, quaerat tempore inventore vitae quam quasi fuga sequi veniam eius porro. Assumenda illum sequi magni laboriosam eligendi quod nobis alias.</p>
+    </article>
+    <article>
+        <h1><a href="/post/my-third-post">My third post</a></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis earum similique, ratione molestiae repudiandae dignissimos quos sapiente ullam possimus non exercitationem laborum necessitatibus asperiores accusamus et saepe praesentium adipisci sequi!
+        Voluptatem ut repellat dolorum, fugiat nemo mollitia iste id, repudiandae, nisi cum odio ipsam aliquid nostrum. Atque rerum fuga, ipsum consectetur commodi temporibus ipsa enim corrupti eveniet dolores placeat perspiciatis!
+        Omnis eaque itaque illo ab quae odio dolorem nesciunt voluptas, quaerat tempore inventore vitae quam quasi fuga sequi veniam eius porro. Assumenda illum sequi magni laboriosam eligendi quod nobis alias.</p>
+    </article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('post/{post}', function($slug) {
+    $post = Post::find($slug);
+    return view('post', compact('post'));
+})->where('post', '[A-z_\-]+');
